f9dd78639230603c15e7e04e4d8327e18e251f11
black2602 <black2602@gmail.com>
2016-03-11 17:32:57 +0900

    스프링공부 플젝 git 에 최초 커밋

---
+++ SpringStudy/src/springbook/learningtest/spring/pointcut/PointCutExpressionTest.java
@@ -0,0 +1,26 @@
+package springbook.learningtest.spring.pointcut;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+public class PointCutExpressionTest {
+	@Test
+	public void methodSignaturePointcut() throws SecurityException, NoSuchMethodException {
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression("execution(public int springbook.learningtest.spring.pointcut.Target.minus(int,int) "
+				+ "throws java.lang.RuntimeException)");
+		
+		assertThat(pointcut.getClassFilter().matches(Target.class) && 
+				pointcut.getMethodMatcher().matches(Target.class.getMethod("minus", int.class, int.class), null), is(true));
+		
+		assertThat(pointcut.getClassFilter().matches(Target.class) && 
+				pointcut.getMethodMatcher().matches(Target.class.getMethod("plus", int.class, int.class), null), is(false));
+		
+		assertThat(pointcut.getClassFilter().matches(Bean.class) && 
+				pointcut.getMethodMatcher().matches(Target.class.getMethod("method"), null), is(false));
+		
+	}
+}
